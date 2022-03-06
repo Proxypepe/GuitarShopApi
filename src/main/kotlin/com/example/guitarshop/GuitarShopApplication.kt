f571b823d2764e019b06d6a07bbf570524c0a34e
Proxypepe <sasha.golovin.51@bk.ru>
2022-03-06 14:37:28 +0300

    initial commit

---
+++ src/main/kotlin/com/example/guitarshop/GuitarShopApplication.kt
@@ -0,0 +1,11 @@
+package com.example.guitarshop
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GuitarShopApplication
+
+fun main(args: Array<String>) {
+    runApplication<GuitarShopApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/guitarshop/GuitarShopApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.guitarshop
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GuitarShopApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
